3ce337b8da6c4f85a7697a4c1e941a6f65181323
NPEDEVCrawfordDI <CrawfordDI@state.gov>
2021-12-16 10:00:51 -0500

    Added files window

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,11 @@ const btnLogin = document.querySelector('.btn-login');
 const btnStart = document.querySelector('.btn-start');
 const btnFile = document.querySelector('.btn-file');
 const btnCloseEmail = document.querySelector('.btn-close-email');
+const btnCloseWindowFiles = document.querySelector('.btn-close-window-files');
+const btnEmail = document.querySelector('.btn-email');
 const dropupContent = document.querySelector('.dropup-content');
 const emailModal = document.querySelector('.modal-email');
+const windowFiles = document.querySelector('.window-files');
 
 const closeLoginModal = function () {
   loginModal.classList.add('hidden');
@@ -20,6 +23,18 @@ const closeLoginModal = function () {
 
 const closeEmail = function() {
   emailModal.classList.add('hidden');
+}
+
+const openEmail = function() {
+  emailModal.classList.remove('hidden');
+}
+
+const closeFilesWindow = function() {
+  windowFiles.classList.add('hidden');
+}
+
+const openFilesWindow = function() {
+  windowFiles.classList.remove('hidden');
 }
 
 btnLogin.addEventListener('click', function () {
@@ -32,8 +47,7 @@ btnLogin.addEventListener('click', function () {
   }
 });
 
-btnFile.addEventListener('click', function() {
-    emailModal.classList.remove('hidden');
-})
-
+btnEmail.addEventListener('click', openEmail);
+btnFile.addEventListener('click', openFilesWindow);
 btnCloseEmail.addEventListener('click', closeEmail);
+btnCloseWindowFiles.addEventListener('click', closeFilesWindow);
